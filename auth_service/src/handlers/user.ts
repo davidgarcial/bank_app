44636add0c9c3f1ce5ee0d50b503870ccb22b0c0
davidgarcial <d.alex10092@gmail.com>
2021-06-28 18:08:51 -0500

    Clean Code

--- auth_service/src/handlers/user.ts
+++ auth_service/src/handlers/user.ts
@@ -1,10 +1,11 @@
 import { Context, helpers } from 'https://deno.land/x/oak/mod.ts';
 import { FindOptions } from "https://deno.land/x/mongo@v0.22.0/mod.ts";
-import { userCollection } from '../db/dbconnect.ts'
+import { userCollection } from '../db/dbconnect.ts';
 
-export const getAllUser = async (ctx: Context) => {
+export const login = async (ctx: Context) => {
   try {
-    const users = await userCollection.find({}, { noCursorTimeout: false } as FindOptions).toArray();
+    const { userId } = helpers.getQuery(ctx, { mergeParams: true });
+    const users = await userCollection.find({ _id: userId }, { noCursorTimeout: false } as FindOptions).toArray();
     ctx.response.body = users;
     ctx.response.status = 200;
   } catch (err) {
@@ -12,35 +13,3 @@ export const getAllUser = async (ctx: Context) => {
     ctx.response.body = { msg: err.message };
   }
 };
-
-/*
-export const findUser = async (ctx: Context) => {
-  const { userId } = helpers.getQuery(ctx, { mergeParams: true });
-  try {
-    const user: User = await db.findUserById(userId);
-    ctx.response.body = user;
-  } catch (err) {
-    ctx.response.status = 404;
-    ctx.response.body = { msg: err.message };
-  }
-};
-
-export const createUser = async (ctx: Context) => {
-  try {
-    const { name, birthDate } = await ctx.request.body().value;
-    const createdUser: User = await db.createUser(name, birthDate);
-    ctx.response.body = createdUser;
-  } catch (err) {
-    ctx.response.status = 500;
-    ctx.response.body = { msg: err.message };
-  }
-};
-
-export const updateUser = async (ctx: Context) => {
-  ctx.response.body = { msg: "User updated!" };
-};
-
-export const deleteUser = async (ctx: Context) => {
-  ctx.response.body = { msg: "User deleted!" };
-};
-*/
--- auth_service/src/routes/index.ts
+++ auth_service/src/routes/index.ts
@@ -1,13 +1,7 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import {
-  getAllUser
+  login
 } from "../handlers/user.ts";
 
 export const router = new Router()
-  .get('/api/users', getAllUser);
-  //User routes
-  /*
-  .get("/api/users/:userId", findUser)
-  .delete("/api/users/:userId", deleteUser)
-  .patch("/api/users", updateUser)
-  .post("/api/users", createUser);*/
+  .get('/api/users', login);

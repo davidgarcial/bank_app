7c5055c4664778fb95ac087584221f208ab63f8a
davidgarcial <d.alex10092@gmail.com>
2021-06-30 22:06:50 -0500

    Apply and validate crypt

--- auth_service/src/handlers/user.ts
+++ auth_service/src/handlers/user.ts
@@ -9,18 +9,19 @@ import { generateJwt } from "../jwt/jwt.ts";
 export const login = async (ctx: Context) => {
   try {
     const { username, password } = await ctx.request.body().value;
-    const pwdCrypt = await hash(password);
-    console.log(pwdCrypt);
     const user = await userCollection.findOne(
       { username: { $eq: username } },
       { noCursorTimeout: false } as FindOptions,
     ) as UserSchema;
 
-    console.log(user.password);
-    const jwt = await generateJwt(user._id.toString());
-
-    ctx.response.body = jwt;
-    ctx.response.status = 200;
+    const pwd = await verify(password, user.password);
+    if (pwd) {
+      const jwt = await generateJwt(user._id.toString());
+      ctx.response.body = jwt;
+      ctx.response.status = 200;
+    } else {
+      throw new Error("The password is wrong.");
+    }
   } catch (err) {
     ctx.response.status = 404;
     ctx.response.body = { msg: err.message };

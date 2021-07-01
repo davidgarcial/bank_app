edc61fb078e7ec1fdaf211a024b5d0a3310e0733
davidgarcial <d.alex10092@gmail.com>
2021-06-30 21:23:55 -0500

    Implement hasj

--- auth_service/src/handlers/user.ts
+++ auth_service/src/handlers/user.ts
@@ -1,8 +1,12 @@
 import { Context } from "https://deno.land/x/oak/mod.ts";
 import { FindOptions } from "https://deno.land/x/mongo@v0.22.0/mod.ts";
+import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
+
 import { userCollection } from "../db/dbconnect.ts";
 import { UserSchema } from "../models/user.ts";
 import { generateJwt } from "../jwt/jwt.ts";
+
+const salt = await bcrypt.genSalt(8);
 
 export const login = async (ctx: Context) => {
   try {
@@ -13,6 +17,24 @@ export const login = async (ctx: Context) => {
     ) as UserSchema;
     const jwt = await generateJwt(_id.toString());
     ctx.response.body = jwt;
+    ctx.response.status = 200;
+  } catch (err) {
+    ctx.response.status = 404;
+    ctx.response.body = { msg: err.message };
+  }
+};
+
+export const createUser = async (ctx: Context) => {
+  try {
+    const { username, password } = await ctx.request.body().value;
+    const hash = await bcrypt.hash(password, salt);
+    const newUser = {
+      user: username,
+      password: hash,
+    };
+
+    await userCollection.insertOne(newUser);
+    ctx.response.body = "";
     ctx.response.status = 200;
   } catch (err) {
     ctx.response.status = 404;
